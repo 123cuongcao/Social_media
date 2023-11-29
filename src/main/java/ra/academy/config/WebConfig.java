fb06973298807ccab80603b1551059083ff15d8c
chuntm <yuand6546@gmail.com>
2023-11-29 11:27:55 +0700

    ui

--- src/main/java/ra/academy/config/WebConfig.java
+++ src/main/java/ra/academy/config/WebConfig.java
@@ -104,7 +104,7 @@ public class WebConfig implements WebMvcConfigurer, ApplicationContextAware {
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
         dataSource.setUrl("jdbc:mysql://localhost:3306/social_media");
         dataSource.setUsername("root");
-        dataSource.setPassword("123456");
+        dataSource.setPassword("chau1");
         return dataSource;
     }
 
